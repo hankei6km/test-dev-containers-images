e08e530c613a86b0799a75c2dc30b3ef37f1ade0
hankei6km <gh-hankei6km@outlook.jp>
2025-04-18 14:51:13 +0000

    chore: Use "tonistiigi/binfmt"

--- scripts/image_setup_builder.sh
+++ scripts/image_setup_builder.sh
@@ -8,7 +8,8 @@ set -u
 docker buildx create --use --name mybuilder
 # binfmt は上記の記事のままだと CodeSpaces で git feature ビルドで停止してしまう(原因は不明) 。
 # あと、deprecated だった。
-docker run --privileged --rm linuxkit/binfmt:ce9509ccfa25002227ccd7ed8dd48d6947854427-amd64
+# docker/setup-qemu-action のデフォルトと同じにしておく(linux/arm64 用イメージもある)。
+docker run --privileged --rm tonistiigi/binfmt:latest --install all
 # arm64 用の binfmt を利用する場合。
 # ただし、手持ちの arm64 環境で builder を作成してのビルドがエラーになるので、実際には試していない。
 # docker run --privileged --rm tonistiigi/binfmt --install all
