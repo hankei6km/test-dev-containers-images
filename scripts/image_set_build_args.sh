3f9cc676f4e1ffdfe9837d3045b07d59ecc4742b
hankei6km <gh-hankei6km@outlook.jp>
2025-05-06 11:46:47 +0000

    chore: Add "-r" option to jq in image_set_build_args.sh

--- scripts/image_set_build_args.sh
+++ scripts/image_set_build_args.sh
@@ -4,5 +4,5 @@ set -u
 set -o pipefail
 
 strip-json-comments <"${1}" |
-    jq --arg baseRepo "${2}" --arg baseTagSuffix "${3}" \
+    jq -r --arg baseRepo "${2}" --arg baseTagSuffix "${3}" \
         '.build.args.BASE_REPO = $baseRepo | .build.args.BASE_TAG_SUFFIX = $baseTagSuffix'
